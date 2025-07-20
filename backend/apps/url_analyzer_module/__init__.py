bc066e9cb13956e69143fa4958bdae6112fa0533
Akash Nandi <akash@focaloid.com>
2025-07-20 11:27:02 +0530

    intial declaration of apps

---
+++ backend/apps/url_analyzer_module/__init__.py
@@ -0,0 +1,1 @@
+""" init file for url_analyzer app """
--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,27 @@
-"""main file for the FastAPI application."""
+"""main file"""
 
 import logging
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from apps.url_analyzer.router import router as url_analyzer_route
+from apps.user_management_module.router import router as user_management_route
+from apps.url_analyzer_module.router import router as url_analyzer_route
 from utils.middlewares import ExceptionMiddleware
 
 # Initialize the FastAPI application
 app = FastAPI()
 
 # define each modules routes here
+app.include_router(user_management_route)
 app.include_router(url_analyzer_route)
 
 # middleware to handle exceptions
+# This middleware will catch exceptions and return a JSON response
+origins = ["http://localhost", "http://localhost:8080", "http://localhost:5173"]
 app.add_middleware(ExceptionMiddleware)
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
