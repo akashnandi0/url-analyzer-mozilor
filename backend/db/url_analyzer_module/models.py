d8cab4b36d70967193f180e785ab34a7413c33ed
Akash Nandi <akash@focaloid.com>
2025-07-20 11:39:11 +0530

    first set of models

---
+++ backend/db/url_analyzer_module/models.py
@@ -0,0 +1,21 @@
+"""Url Analysis Models"""
+
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON
+from sqlalchemy.orm import relationship
+from db.database import Base
+from db.user_management.models import User
+
+
+class URLAnalysis(Base):
+    """URL Analysis model class"""
+
+    __tablename__ = "url_analysis"
+
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String)
+    word_frequencies = Column(JSON)
+    created_at = Column(DateTime, default=datetime.now)
+    user_id = Column(Integer, ForeignKey(User.id))
+
+    user = relationship("User", back_populates="analysis")
